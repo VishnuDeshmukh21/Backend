51bcf2e5483f32229ff2c6c1e4186da8644dd33e
Vishnu Deshmukh <vishnudeshmukh1009@gmail.com>
2023-09-19 03:20:59 +0530

    Host

--- mal_detection/settings.py
+++ mal_detection/settings.py
@@ -137,3 +137,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# settings.py
+
+ALLOWED_HOSTS = ['44.198.44.136']
